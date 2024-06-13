f071093b9eeb5756c858ee1b9e37fde8ce87ebb4
Hemel Parody <hemelparody@hotmail.com>
2024-06-13 00:25:36 -0500

    Feature: Create Price Input

---
+++ src/components/ui/PriceInput.jsx
@@ -0,0 +1,28 @@
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import { numberInputClasses } from "@mui/base/Unstable_NumberInput";
+import NumberInput from "./NumberInput";
+import { styled } from "@mui/material";
+
+const InputAdornment = styled("div")(
+  ({ theme }) => `
+  margin: 8px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  grid-row: 1/3;
+  color: ${theme.palette.primary.light};
+`
+);
+
+const PriceInput = (props) => (
+  <NumberInput
+    startAdornment={
+      <InputAdornment>
+        <AttachMoneyIcon fontSize="medium" />
+      </InputAdornment>
+    }
+    {...props}
+  />
+);
+
+export default PriceInput;
