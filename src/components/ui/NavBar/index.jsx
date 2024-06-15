a3a5915c349a145e2cefe74493474f27940dd2dd
Hemel Parody <hemelparody@hotmail.com>
2024-06-14 19:41:43 -0500

    Feature: add logout feature

--- src/components/ui/NavBar/index.jsx
+++ src/components/ui/NavBar/index.jsx
@@ -1,8 +1,20 @@
-import { useMemo } from "react";
+import { useState, useMemo, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "@emotion/styled";
-import { AppBar, Avatar, Toolbar, Divider, Box, Button } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Toolbar,
+  Divider,
+  Box,
+  Button,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  styled,
+  Typography,
+} from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
+import Logout from "@mui/icons-material/Logout";
 
 import Logo from "@/assets/vane-store.png";
 import useAuth from "@/hooks/useAuth";
@@ -10,7 +22,7 @@ import useAuth from "@/hooks/useAuth";
 import NavLink from "./NavLink";
 import ShoppingCart from "./ShoppingCart";
 
-const LogoImg = styled.img`
+const LogoImg = styled("img")`
   aspect-ratio: inherit;
   object-fit: contain;
   width: 40px;
@@ -22,7 +34,19 @@ const LogoImg = styled.img`
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const { user, isAdmin, isLoggedIn } = useAuth();
+  const { user, isAdmin, isLoggedIn, logout } = useAuth();
+  const [anchorMenuElement, setAnchorMenuElement] = useState();
+  const openAvatarMenu = useMemo(
+    () => Boolean(anchorMenuElement),
+    [anchorMenuElement]
+  );
+  const onClickAvatarMenu = (event) => {
+    setAnchorMenuElement(event.currentTarget);
+  };
+
+  const onCloseAvatarMenu = () => {
+    setAnchorMenuElement(null);
+  };
 
   const userInitial = useMemo(() => {
     if (isLoggedIn) {
@@ -64,13 +88,47 @@ const NavBar = () => {
               variant="outlined"
               size="small"
               startIcon={<PersonIcon />}
-              sx={{ padding: "5px 20px" }}
+              sx={{ padding: "5px 20px", marginRight: "10px" }}
             >
               Iniciar Sesión
             </Button>
           )}
           <ShoppingCart sx={{ marginRight: "10px" }} />
-          {isLoggedIn && <Avatar>{userInitial}</Avatar>}
+          {isLoggedIn && (
+            <Fragment>
+              <Avatar
+                id="id_avatar"
+                onClick={onClickAvatarMenu}
+                sx={{ cursor: "pointer" }}
+              >
+                {userInitial}
+              </Avatar>
+              <Menu
+                id="id_avatar_menu"
+                anchorEl={anchorMenuElement}
+                open={openAvatarMenu}
+                onClose={onCloseAvatarMenu}
+                MenuListProps={{
+                  "aria-labelledby": "id_avatar",
+                }}
+              >
+                <MenuItem>
+                  <ListItemIcon>
+                    <Avatar id="id_profile" sx={{ width: 24, height: 24 }}>
+                      <Typography variant="body2">{userInitial}</Typography>
+                    </Avatar>
+                  </ListItemIcon>
+                  Profile
+                </MenuItem>
+                <MenuItem onClick={() => logout()}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Cerrar Sesión
+                </MenuItem>
+              </Menu>
+            </Fragment>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
